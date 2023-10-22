b4f7ced0d65c277f92ad2337e5abf0b782a1dae9
kosenko.dev <d.kosenko@qiwi.com>
2023-10-22 15:03:22 +0300

    #19 Основные сценарии использования Playwright

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,66 +1,40 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.youtube.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/YouTube/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('try library link', async ({ page }) => {
+  await page.goto('https://www.youtube.com/');
 
-  await page.locator('#userName')
+  await page.getByRole('link', { name: 'Library' }).click({delay: 300});
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Library/);
 });
 
-test('Success login', async ({ page }) => {
-  await page.goto('/login');
-  await page.getByPlaceholder('UserName').fill('testdamir');
-  await page.getByPlaceholder('Password').fill('testdamir_D0&');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByText('User Name : testdamirLog out').isVisible();
+test('try subscriptions link', async ({ page }) => {
+  await page.goto('https://www.youtube.com/');
+
+  await page.getByRole('link', { name: 'Subscriptions' }).click({delay: 300});
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Subscriptions/);
 });
 
-test('Cannot auth without credentials', async ({ page }) => {
-  await page.goto('/login');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.waitForLoadState('networkidle')
-  await expect(page.getByPlaceholder('UserName')).toHaveClass(/is\-invalid/)
-  await expect(page.getByPlaceholder('Password')).toHaveClass(/is\-invalid/)
+test('try to make search', async ({ page }) => {
+  await page.goto('https://www.youtube.com/');
+  await page.getByPlaceholder('Search').fill('cyber cat studio');
+  await page.getByRole('button', { name: 'Search', exact: true}).click({delay: 300});
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/cyber cat studio/);
 });
 
-test('Success add book to collection', async ({ page }) => {
-  await page.route('**/BookStore/v1/Books', route => {
-    route.fulfill({
-      status: 201,
-      body: JSON.stringify({
-        "userId":"45057726-ddf4-41ec-ae30-978a936d0495",
-        "collectionOfIsbns":[{"isbn":"9781491950296"}]
-      })
-    })
-  })
-
-  await page.goto('/login');
-  await page.getByPlaceholder('UserName').fill('testdamir');
-  await page.getByPlaceholder('Password').fill('testdamir_D0&');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByText('User Name : testdamirLog out').isVisible();
-  await page.waitForLoadState('networkidle')
-  await page.goto('/books?book=9781491950296');
-  page.on('dialog', async dialog => {
-    expect(dialog.type()).toContain('alert')
-    expect(dialog.message()).toBe('Book added to your collection.')
-    await dialog.accept()
-  })
-  await page.getByRole('button', { name: 'Add To Your Collection' }).click()
-  await page.waitForLoadState('networkidle')
-  // Book added to your collection.
-  // 
-  // 
-})
+test('try to go right into short', async ({ page }) => {
+  await page.goto('/shorts/4bYnY5Sxc6E');
+  await expect(page).toHaveTitle(/crab dance/);
+});
